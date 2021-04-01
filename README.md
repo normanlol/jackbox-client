eb3de67cce2a9e4b998e49bd5c9b910f7e9d03c5
norman <41760554+n0rmancodes@users.noreply.github.com>
2021-04-01 00:07:15 -0400

    wooooo first version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # jackbox-client
-Simple client for Jackbox Games.
+Simple client for Jackbox Games based on Electron.
+
+This is mainly to just experiment with Electron and to use something besides my phone/browsers to play Jackbox.
+
+## screenshots
+
+![Main menu](/assets/main.png)
+
+*Main menu of Jackbox.tv*
---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, shell } = require('electron');
+
+app.whenReady().then(() => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "Jackbox"
+    });
+    win.setMenu(null);
+    win.loadURL("https://jackbox.tv/", {userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0"});
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length == 0) {
+            const win = new BrowserWindow({
+                width: 800,
+                height: 600,
+                title: "Jackbox"
+            });
+            win.setMenu(null);
+            win.loadURL("https://jackbox.tv/", {userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0"});
+        }
+    });
+
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
+    
+    win.webContents.addListener("will-navigate", function(event, url) {
+        if (!url.startsWith("https://jackbox.tv/")) {
+            event.preventDefault();
+            shell.openExternal(url);
+        }
+    });
+    
+    win.webContents.addListener("new-window", function(event, url) {
+        event.preventDefault();
+        shell.openExternal(url);
+    });
+});
+
